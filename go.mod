8a4628a15dfc8a01bbb6a9f2690209ab3779bbcd
Jesse Brown <jabrown85@gmail.com>
2020-06-02 09:28:47 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jabrown85/notify-benchmark
+
+go 1.13
+
+require github.com/rjeczalik/notify v0.9.2
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/rjeczalik/notify"
+)
+
+func main() {
+	c := make(chan notify.EventInfo, 1)
+
+	if err := notify.Watch(".", c, notify.All); err != nil {
+		log.Fatal(err)
+	}
+	defer notify.Stop(c)
+
+	for {
+		select {
+		case ei := <-c:
+			info, _ := os.Stat(ei.Path())
+			log.Printf("Duration for %s(%s): %s", ei.Path(), ei.Event().String(), time.Since(info.ModTime()))
+		}
+	}
+}
